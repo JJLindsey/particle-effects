63a8759708589eaa16e857b35e1b53cda435ec3a
JJLindsey <jlindsey010@gmail.com>
2021-10-12 17:50:49 -0700

    add size and velocity & loop thru particles

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let p;
+const particles = [];
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
     //console.log(width);
-    p = new Particle();
+    const particlesLength = Math.floor(window.innerWidth / 10 );
     
+    for (let i = 0; i < particlesLength; i++) {
+        particles.push(new Particle());
+
+    }
 }
 
 function draw() {
@@ -16,27 +20,40 @@ function draw() {
     
     // circle(mouseX, mouseY, 80);
     background(55, 100, 144);
-    p.update();
-    p.draw();
+    particles.forEach((p, index) => {
+        p.update();
+        p.draw();
+    })
 }
 
 class Particle {
     constructor() {
         //position
         this.pos = createVector(random((width)), random(height));
-        //velocity
+        //velocity larger numbers faster movement
         this.vel = createVector(random(-2, 2), random(-2, 2));
         //size
         this.size = 10;
     }
-
+//update movment by adding velocity
     update() {
         this.pos.add(this.vel);
+        this.edges();
     }
-
+//draw single particle
     draw() {
         noStroke();
         fill('rgba(255, 255, 255, 0.5)');
         circle(this.pos.x, this.pos.y, this.size);
+    }
+    //detect edge
+    edges() {
+        if(this.pos.x < 0 || this.pos.x > width) {
+            this.vel.x *= -1;
+        }
+
+        if(this.pos.y < 0 || this.pos.y > height) {
+            this.vel.y *= -1;
+        }
     }
 }
